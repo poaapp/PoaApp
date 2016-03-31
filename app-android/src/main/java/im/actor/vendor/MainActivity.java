3606c6b2b8701552572ee4ebea1dc32a126f26d9
kor_ka <badgrib@gmmail.com>
2016-03-31 17:12:07 +0300

    feat(android): add poa app

--- app-android/src/main/java/im/actor/vendor/MainActivity.java
+++ app-android/src/main/java/im/actor/vendor/MainActivity.java
@@ -4,13 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import im.actor.sdk.ActorSDK;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         ActorSDK.sharedActor().startMessagingApp(this);
         finish();
     }
--- app-android/src/main/java/im/actor/vendor/MessengerApplication.java
+++ app-android/src/main/java/im/actor/vendor/MessengerApplication.java
@@ -2,6 +2,10 @@ package im.actor.vendor;
 
 import com.crashlytics.android.Crashlytics;
 
+import android.graphics.Color;
+
+import com.crashlytics.android.Crashlytics;
+
 import im.actor.sdk.ActorSDK;
 import im.actor.sdk.ActorSDKApplication;
 import im.actor.sdk.ActorStyle;
@@ -10,14 +14,21 @@ import io.fabric.sdk.android.Fabric;
 public class MessengerApplication extends ActorSDKApplication {
 
     @Override
-    public void onCreate() {
-        Fabric.with(this, new Crashlytics());
-        super.onCreate();
+    public void onConfigureActorSDK() {
+        // Overriding app name
+
+        ActorSDK.sharedActor().setAppName(getString(R.string.app_vendor_name));
+        ActorSDK.sharedActor().setCustomApplicationName(getString(R.string.app_vendor_name));
+        ActorSDK.sharedActor().setEndpoints(new String[]{"tcp://app.poaapp.co.tz:9070"});
+
+        ActorStyle style = ActorSDK.sharedActor().style;
+        style.setMainColor(Color.parseColor("#ED1C24"));
     }
 
     @Override
-    public void onConfigureActorSDK() {
-        // Overriding app name
-        ActorSDK.sharedActor().setAppName(getString(R.string.app_name));
+    public void onCreate() {
+        super.onCreate();
+        Fabric.with(this, new Crashlytics());
+
     }
 }
